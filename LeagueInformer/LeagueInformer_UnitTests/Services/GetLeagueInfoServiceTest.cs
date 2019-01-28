68d4ed6d078ad6055bd53b6ffdee7f4e43f49bd1
Filip Nowicki <filip.nowicki1999@gmail.com>
2019-01-28 10:27:29 +0100

    Add GetLeagueInfoService Test

---
+++ LeagueInformer/LeagueInformer_UnitTests/Services/GetLeagueInfoServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using LeagueInformer.Api.Interfaces;
+using LeagueInformer.Models;
+using LeagueInformer.Services;
+using Moq;
+using Xunit;
+
+namespace LeagueInformer_UnitTests.Services
+{
+    public class GetLeagueInfoServiceTest
+    {
+        private readonly Mock<IApiClient> _GetJson;
+        private readonly GetLeagueInfoService _GetLeagueInfo;
+        public GetLeagueInfoServiceTest()
+        {
+            _GetJson = new Mock<IApiClient>();
+            _GetLeagueInfo = new GetLeagueInfoService(_GetJson.Object);
+        }
+
+        [Fact]
+        public async void JsonResponseIsNull()
+        {
+            //Arrange
+            _GetJson.Setup(x => x.GetJsonFromUrl("abc")).ReturnsAsync(string.Empty);
+
+            //Act
+            var result = await _GetLeagueInfo.GetListOfSummonerLeague("", new LeagueOfSummoner(), "");
+
+            //Assert
+            Assert.False(result.IsSuccess);
+        }
+    }
+}
